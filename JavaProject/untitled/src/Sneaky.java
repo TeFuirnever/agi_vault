eafad4d97a407442285cc47f3479d8899b7cc1f7
504897664@qq.com <GuanXL199628..>
2026-02-04 22:44:11 +0800

    vault backup: 2026-02-04 22:44:11

--- JavaProject/untitled/src/Sneaky.java
+++
@@ -1,6 +0,0 @@
-public class Sneaky {
-    public static void main(String[] args) {
-        // 下面这行看起来是注释，其实会打印
-        // \u000d System.out.println("我是一行被注释的代码！");
-    }
-}
--- JavaProject/untitled/src/UrlTrick.java
+++
@@ -1,8 +0,0 @@
-public class UrlTrick {
-    public static void main(String[] args) {
-        
-        http://www.baidu.com
-        
-        System.out.println("刚才那行没报错");
-    }
-}
